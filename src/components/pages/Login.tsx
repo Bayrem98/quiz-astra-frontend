f058481e52633ba032e97c77a4372f58f550d12a
bayrem98 <bayremsayeh98@gmail.com>
2023-12-12 17:50:48 +0100

    update code & css value for login file

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -2,7 +2,7 @@ import React, { ChangeEvent, useState } from "react";
 import { faEye, faLock, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
-import { Button, Card, Form, FormGroup, Input, Label } from "reactstrap";
+import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 import { Alert } from "antd";
 
 function Login() {
@@ -57,114 +57,114 @@ function Login() {
 
   return (
     <>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <Card style={{ width: 900, height: 700, marginTop: 80 }}>
-          <div style={{ display: "flex", height: "100vh" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <div style={{ display: "flex", height: "100vh" }}>
+          <div style={{ flex: 1, padding: "20px" }}>
+            <img
+              src="/image/pngwing.com.png"
+              alt="."
+              style={{ height: 350, width: 350, marginTop: 170 }}
+            />
+          </div>
+          <div
+            style={{
+              flex: 1,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              marginBottom: 120,
+            }}
+          >
             <div
-              style={{ flex: 1, backgroundColor: "#f0f0f0", padding: "20px" }}
-            >
-              <img
-                src="/pngwing.com.png"
-                alt="."
-                style={{ height: 400, width: 357, marginTop: 130 }}
-              />
-            </div>
-            <div
+              className="card"
               style={{
-                flex: 1,
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                backgroundColor: "#f0f0f0",
-                marginRight: 50,
+                backgroundColor: "lightgray",
+                width: 500,
+                borderRadius: 20,
               }}
             >
-              <div
-                className="card"
-                style={{
-                  backgroundColor: "#ddd9d5",
-                  width: 500,
-                  borderRadius: 20,
-                }}
-              >
-                <br />
-                {errorMessage && (
-                  <Alert
-                    message=""
-                    description={errorMessage}
-                    type="error"
-                    showIcon
-                  />
-                )}
+              <br />
+              {errorMessage && (
+                <Alert
+                  message=""
+                  description={errorMessage}
+                  type="error"
+                  showIcon
+                />
+              )}
 
-                <h2 style={{ color: "black", textAlign: "center" }}>
-                  Connectez-vous
-                </h2>
-                <Form onSubmit={(event) => login(event)}>
-                  <FormGroup style={{ paddingLeft: 95 }}>
-                    <Label style={{ color: "black" }}>
-                      <FontAwesomeIcon
-                        icon={faUser}
-                        beatFade
-                        style={{ color: "#000000", marginRight: 5 }}
-                      />
-                      Nom d'utilisateur
-                    </Label>
-                    <div className="d-flex justify-content-between">
-                      <Input
-                        type="text"
-                        value={username}
-                        onChange={handleUsernameChange}
-                        style={{ width: 300 }}
-                      />
-                    </div>
-                  </FormGroup>
-                  <FormGroup style={{ paddingLeft: 95 }}>
-                    <Label style={{ color: "black" }}>
-                      <FontAwesomeIcon
-                        icon={faLock}
-                        beatFade
-                        style={{ color: "#000000", marginRight: 5 }}
-                      />
-                      Mot de passe
-                    </Label>
-                    <div className="d-flex justify-content-between">
-                      <Input
-                        value={password}
-                        onChange={handlePasswordChange}
-                        type={passwordShown ? "text" : "password"}
-                        style={{ width: 300 }}
-                      />
-                      <Button
-                        style={{
-                          color: "black",
-                          cursor: "pointer",
-                          position: "relative",
-                          right: 60,
-                          backgroundColor: "beige",
-                          border: 0,
-                        }}
-                        onClick={togglePasswordVisiblity}
-                      >
-                        {eye}
-                      </Button>
-                    </div>
-                  </FormGroup>
-                  <FormGroup style={{ textAlign: "center" }}>
+              <h2 style={{ color: "black", textAlign: "center" }}>
+                Connectez-vous
+              </h2>
+              <Form onSubmit={(event) => login(event)}>
+                <FormGroup style={{ paddingLeft: 95 }}>
+                  <Label style={{ color: "black" }}>
+                    <FontAwesomeIcon
+                      icon={faUser}
+                      beatFade
+                      style={{ color: "#000000", marginRight: 5 }}
+                    />
+                    Nom d'utilisateur
+                  </Label>
+                  <div className="d-flex justify-content-between">
+                    <Input
+                      type="text"
+                      value={username}
+                      onChange={handleUsernameChange}
+                      style={{ width: 300 }}
+                    />
+                  </div>
+                </FormGroup>
+                <FormGroup style={{ paddingLeft: 95 }}>
+                  <Label style={{ color: "black" }}>
+                    <FontAwesomeIcon
+                      icon={faLock}
+                      beatFade
+                      style={{ color: "#000000", marginRight: 5 }}
+                    />
+                    Mot de passe
+                  </Label>
+                  <div className="d-flex justify-content-between">
+                    <Input
+                      value={password}
+                      onChange={handlePasswordChange}
+                      type={passwordShown ? "text" : "password"}
+                      style={{ width: 300 }}
+                    />
                     <Button
-                      size="lg"
-                      style={{ backgroundColor: "beige", cursor: "pointer" }}
-                      type="submit"
-                      disabled={!username || !password}
+                      style={{
+                        color: "black",
+                        cursor: "pointer",
+                        position: "relative",
+                        right: 60,
+                        backgroundColor: "beige",
+                        border: 0,
+                      }}
+                      onClick={togglePasswordVisiblity}
                     >
-                      <span style={{ color: "black" }}>Valider</span>
+                      {eye}
                     </Button>
-                  </FormGroup>
-                </Form>
-              </div>
+                  </div>
+                </FormGroup>
+                <FormGroup style={{ textAlign: "center" }}>
+                  <Button
+                    size="lg"
+                    style={{ backgroundColor: "beige", cursor: "pointer" }}
+                    type="submit"
+                    disabled={!username || !password}
+                  >
+                    <span style={{ color: "black" }}>Valider</span>
+                  </Button>
+                </FormGroup>
+              </Form>
             </div>
           </div>
-        </Card>
+        </div>
       </div>
     </>
   );
