b5a3b3524c3629cea86d0202f33ebefe722096e6
bayrem98 <bayremsayeh98@gmail.com>
2023-12-29 16:45:42 +0100

    add input for filter users in recapall

--- src/components/Administrateur/recap-users-admin/RecapAllUsers.tsx
+++ src/components/Administrateur/recap-users-admin/RecapAllUsers.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Button, ButtonGroup, Table } from "reactstrap";
+import { Button, ButtonGroup, Input, Table } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBoxOpen } from "@fortawesome/free-solid-svg-icons";
 import User from "../../../@types/User";
@@ -11,6 +11,7 @@ interface Props {}
 
 const RecapAllUsers = (props: Props) => {
   const [users, setUsers] = useState<User[]>([]);
+  const [filter, setFilter] = useState<string>("");
 
   useEffect(() => {
     getUsers(setUsers);
@@ -27,9 +28,21 @@ const RecapAllUsers = (props: Props) => {
         className="users-table-container"
         style={{ paddingTop: 80, paddingLeft: 25, paddingRight: 25 }}
       >
-        <h3 style={{ color: "white", textAlign: "center" }}>
-          Recap des Utilisateurs
-        </h3>
+        <div className="d-flex justify-content-between">
+          <div>
+            <h3 style={{ color: "white", textAlign: "center" }}>
+              Recap des Utilisateurs
+            </h3>
+          </div>
+          <div style={{ marginRight: 200 }}>
+            <Input
+              type="text"
+              placeholder="Chercher içi..."
+              value={filter}
+              onChange={(e) => setFilter(e.target.value)}
+            />
+          </div>
+        </div>
         <br />
         <div style={{ marginLeft: 50, marginRight: 50, paddingBottom: 595 }}>
           <Table bordered responsive hover>
@@ -41,25 +54,29 @@ const RecapAllUsers = (props: Props) => {
             </thead>
             <tbody>
               {users.length ? (
-                users.map((user) => (
-                  <tr key={user._id}>
-                    <td>{user.username}</td>
-                    <td style={{ textAlign: "center" }}>
-                      <ButtonGroup>
-                        <Link to={`/recaponeuser/${user._id}`}>
-                          <Button>
-                            <FontAwesomeIcon
-                              icon={faEye}
-                              color="white"
-                              beat
-                              size="xl"
-                            />
-                          </Button>
-                        </Link>
-                      </ButtonGroup>
-                    </td>
-                  </tr>
-                ))
+                users
+                  .filter((user) =>
+                    user.username.toLowerCase().includes(filter.toLowerCase())
+                  )
+                  .map((user) => (
+                    <tr key={user._id}>
+                      <td>{user.username}</td>
+                      <td style={{ textAlign: "center" }}>
+                        <ButtonGroup>
+                          <Link to={`/recaponeuser/${user._id}`}>
+                            <Button>
+                              <FontAwesomeIcon
+                                icon={faEye}
+                                color="white"
+                                beat
+                                size="xl"
+                              />
+                            </Button>
+                          </Link>
+                        </ButtonGroup>
+                      </td>
+                    </tr>
+                  ))
               ) : (
                 <tr>
                   <td colSpan={2} className="text-center">
