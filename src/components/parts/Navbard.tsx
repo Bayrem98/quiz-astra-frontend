8390a3b28ffd178ef6822a1f7897237936ac3333
bayrem98 <bayremsayeh98@gmail.com>
2023-12-29 14:06:48 +0100

    update code navbar file remove many function for login & logout admin

--- src/components/parts/Navbard.tsx
+++ src/components/parts/Navbard.tsx
@@ -1,27 +1,14 @@
-import React, { ChangeEvent, useEffect, useState } from "react";
-import { faEye } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Alert } from "antd";
-import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import {
-  Button,
   DropdownItem,
   DropdownMenu,
   DropdownToggle,
-  Form,
-  FormGroup,
-  Input,
-  Label,
-  Modal,
-  ModalBody,
-  ModalFooter,
-  ModalHeader,
   Navbar,
   NavbarBrand,
   UncontrolledDropdown,
 } from "reactstrap";
-import { v4 as uuidv4 } from "uuid";
+
 import Cookies from "js-cookie";
 import User from "../../@types/User";
 import { getUser } from "../../actions/Users/action";
@@ -29,105 +16,30 @@ import Adminuser from "../../@types/Adminuser";
 import { getAdminuser } from "../../actions/Adminusers/action";
 
 function Navbard() {
-  const [isOpened, setIsOpened] = useState(false);
-  const [username, setUsername] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [passwordShown, setPasswordShown] = useState(false);
-  const [errorMessage, setErrorMessage] = useState<string>("");
   const [userData, setUserData] = useState<User | null>(null);
   const [adminuser, setAdminuser] = useState<Adminuser | null>(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (userId) {
       getUser(userId, setUserData);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
     if (userId) {
       getAdminuser(userId, setAdminuser);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  const togglePasswordVisiblity = () => {
-    setPasswordShown(passwordShown ? false : true);
-  };
-  const eye = <FontAwesomeIcon icon={faEye} />;
-
-  const changeUsername = (e: ChangeEvent<HTMLInputElement>) =>
-    setUsername(e.target.value);
-
-  const changePassword = (e: ChangeEvent<HTMLInputElement>) =>
-    setPassword(e.target.value);
-
-  const navigate = useNavigate();
-
-  const toggleModal = (e: any) => {
-    setIsOpened(!isOpened);
-    e.preventDefault();
-  };
-
-  const handelClose = () => setIsOpened(false);
-
-  const handleReset = () => {
-    setUsername("");
-    setPassword("");
-    handelClose();
-  };
-
-  const generateRandomTokenValue = () => {
-    const token = uuidv4();
-    return token;
-  };
-
-  const token = generateRandomTokenValue();
-
-  const logina = (e: any) => {
-    e.preventDefault();
-    axios
-      .post(`http://localhost:3000/auth/logina`, {
-        username,
-        password,
-      })
-      .then(({ data }) => {
-        Cookies.set(
-          "access_token",
-          token,
-          { expires: 5 / 24 } // 1 heure (1/24 de la journée)
-        );
-        window.location.reload();
-        handelClose();
-        navigateto();
-        console.log(data);
-      })
-      .catch((error) => {
-        if (
-          error.response &&
-          error.response.data &&
-          error.response.data.message
-        ) {
-          setErrorMessage(error.response.data.message);
-        } else {
-          setErrorMessage("Une erreur s'est produite. Veuillez réessayer.");
-        }
-      });
-  };
-
-  const navigateto = () => {
-    window.location.replace("/questionstable");
-  };
 
   const logout = () => {
     localStorage.removeItem("access_token");
     localStorage.removeItem("user_id");
     localStorage.removeItem("quiz_responses");
-    navigate("/");
-    window.location.reload();
-  };
-
-  const logoutAdmin = () => {
     Cookies.remove("access_token");
-    navigate("/home");
+    navigate("/");
     window.location.reload();
   };
 
@@ -194,32 +106,9 @@ function Navbard() {
                   </DropdownItem>
                 </DropdownMenu>
               </UncontrolledDropdown>
-              <Button
-                style={{
-                  marginLeft: 15,
-                  border: 0,
-                  backgroundColor: "white",
-                  color: "#6c757d",
-                }}
-                onClick={logoutAdmin}
-              >
-                Logout-Admin
-              </Button>
             </>
           ) : (
-            <>
-              <span
-                style={{
-                  textDecoration: "none",
-                  color: "white",
-                  fontSize: 18,
-                  cursor: "pointer",
-                }}
-                onClick={toggleModal}
-              >
-                Admin-Session
-              </span>
-            </>
+            <></>
           )}
         </div>
         <UncontrolledDropdown inNavbar>
@@ -265,123 +154,6 @@ function Navbard() {
           </DropdownMenu>
         </UncontrolledDropdown>
       </Navbar>
-
-      {isOpened ? (
-        <Modal
-          centered
-          scrollable
-          isOpen={isOpened}
-          toggle={() => setIsOpened(false)}
-        >
-          <Form onSubmit={(e) => logina(e)}>
-            <ModalHeader
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                backgroundColor: "#6c757d",
-              }}
-            >
-              {errorMessage && (
-                <Alert
-                  message=""
-                  description={errorMessage}
-                  type="error"
-                  showIcon
-                  style={{
-                    width: "auto",
-                    minWidth: "5ch",
-                    maxWidth: "60ch",
-                  }}
-                />
-              )}
-            </ModalHeader>
-            <ModalBody>
-              <span
-                className="modal-admin-closeicon"
-                onClick={handelClose}
-                style={{
-                  position: "relative",
-                  top: 0,
-                  left: 460,
-                  cursor: "pointer",
-                  fontSize: 20,
-                  color: "#6c757d",
-                }}
-              >
-                X
-              </span>
-              <p
-                className="modal-admin-title"
-                style={{
-                  textAlign: "center",
-                  fontWeight: "bold",
-                  color: "#6c757d",
-                  fontSize: 20,
-                }}
-              >
-                Admin-connecte
-              </p>
-
-              <FormGroup floating>
-                <Input
-                  value={username}
-                  id="username"
-                  name="username"
-                  type="text"
-                  onChange={changeUsername}
-                />
-                <Label className="modal-admin-label-text" for="username">
-                  Username
-                </Label>
-              </FormGroup>
-              <FormGroup floating>
-                <Input
-                  value={password}
-                  id="password"
-                  name="password"
-                  type={passwordShown ? "text" : "password"}
-                  onChange={changePassword}
-                />
-                <i
-                  className="modal-admin-eyeicon"
-                  onClick={togglePasswordVisiblity}
-                  style={{
-                    position: "relative",
-                    left: 435,
-                    bottom: 38,
-                    color: "black",
-                    cursor: "pointer",
-                  }}
-                >
-                  {eye}
-                </i>
-                <Label className="modal-admin-label-text" for="password">
-                  Password
-                </Label>
-              </FormGroup>
-            </ModalBody>
-            <ModalFooter>
-              <Button
-                className="modal-admin-button1"
-                type="submit"
-                disabled={!username || !password}
-                style={{ border: 0 }}
-              >
-                Confirm
-              </Button>
-              <Button
-                className="modal-admin-button2"
-                onClick={handleReset}
-                style={{ backgroundColor: "black", border: 0 }}
-              >
-                Annuler
-              </Button>
-            </ModalFooter>
-          </Form>
-        </Modal>
-      ) : (
-        <></>
-      )}
     </>
   );
 }
